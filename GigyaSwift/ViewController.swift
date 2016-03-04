5a59d4b9d48da5b6e8a013b7aa0cf14454284f34
NRasool <nrasool.developer@gmail.com>
2016-03-04 11:43:09 +0000

    first version

---
+++ GigyaSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GigyaSwift
+//
+//  Created by NRasool on 04/03/2016.
+//  Copyright © 2016 NRasool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
