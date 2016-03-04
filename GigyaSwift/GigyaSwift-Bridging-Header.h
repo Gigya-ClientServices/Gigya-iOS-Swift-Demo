a7e2454f66b4510b79dca6b31662e8fa0244a465
NRasool <nrasool.developer@gmail.com>
2016-03-04 16:32:58 +0000

    Added Gigya Framework and swift bridge to point to the Gigya header files

---
+++ GigyaSwift/GigyaSwift-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  GigyaSwift-Bridging-Header.h
+//  GigyaSwift
+//
+//  Created by NRasool on 04/03/2016.
+//  Copyright © 2016 NRasool. All rights reserved.
+//
+
+#ifndef GigyaSwift_Bridging_Header_h
+#define GigyaSwift_Bridging_Header_h
+
+
+#endif /* GigyaSwift_Bridging_Header_h */
+
+#import "GigyaSDK.framework/Versions/A/Headers/Gigya.h"
